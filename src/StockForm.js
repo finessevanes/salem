a8d94e352f15ed1752a4625dfbdee9a2383af4c3
Vanessa Mercado <vanessa.mercado24@gmail.com>
2020-05-08 12:25:54 -0700

    added chart, historical performance button

--- src/StockForm.js
+++ src/StockForm.js
@@ -1,11 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import "./App.css";
+import { LineChart, Line, Label, XAxis, YAxis, Tooltip } from "recharts";
+import moment from "moment";
 
 const StockForm = () => {
   const [tickerSymbol, setTickerSymbol] = useState("");
   const [stock, setStock] = useState([]);
-
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [performanceData, setPerformanceData] = useState("");
   // const API_CALL = `https://financialmodelingprep.com/api/v3/quote/${tickerSymbol}`;
   const API_CALL = `https://financialmodelingprep.com/api/v3/historical-price-full/${tickerSymbol}?serietype=line`;
+  const { symbol } = stock;
+  const historical = stock.historical;
+  const { historical: stockStats } = stock;
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,6 +20,7 @@ const StockForm = () => {
       .then((res) => res.json())
       .then((stock) => {
         setStock(stock);
+        setIsSubmitted(true);
       });
   };
 
@@ -21,23 +29,17 @@ const StockForm = () => {
     setTickerSymbol(value);
   };
 
-  const { symbol } = stock;
-  const historical = stock.historical;
+  const handleHP = () => {
+    let performanceData = stockStats.map((instance, i) => (
+      <li key={stockStats[i].date}>
+        Date: {stockStats[i].date} Close: {stockStats[i].close}
+      </li>
+    ));
+    setPerformanceData(performanceData);
+  };
 
   return (
-    <div>
-      {symbol}
-      <br />
-      {stock.historical &&
-        historical.map((item) => (
-          <p>
-            Closing price: ${item.close}
-            Date: {item.date}
-          </p>
-        ))}
-      <br />
-      <br />
-      <br />
+    <div className="stockForm">
       <form onSubmit={handleSubmit}>
         <input
           value={tickerSymbol}
@@ -46,8 +48,34 @@ const StockForm = () => {
         />
         <input type="submit" value="Search" />
       </form>
+      <h1>{symbol}</h1>
+      <LineChart width={800} height={400} data={historical}>
+        <Line type="monotone" stroke="#8884d8" dataKey="close" />
+        <XAxis
+          dataKey="date"
+          tickFormatter={(unixTime) => moment(unixTime).format("MM YY")}
+        />
+        <Tooltip />
+        {stock.historical && (
+          <YAxis stroke="blue" axisLine={{ stroke: "#EAF0F4" }}>
+            <Label
+              value={"Price"}
+              position="insideLeft"
+              angle={-90}
+              style={{ textAnchor: "middle" }}
+            />
+          </YAxis>
+        )}
+      </LineChart>
+      <br />
+      <br />
+      <ul>
+        {isSubmitted && (
+          <button onClick={handleHP}>Historical Performance</button>
+        )}
+        {performanceData}
+      </ul>
     </div>
   );
 };
-
 export default StockForm;
